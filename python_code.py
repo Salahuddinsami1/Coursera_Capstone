e7b41702529a33a1460d6bd099c5e13bc85fbfb5
Salahuddinsami1 <salahuddin2110.cu@gmail.com>
2019-08-12 02:16:48 +0600

    adds a python script for directing to Capstone Notebook

---
+++ python_code.py
@@ -0,0 +1,1 @@
+print “https://dataplatform.cloud.ibm.com/analytics/notebooks/v2/b98b218f-4492-4a29-842a-6eddc479001b/view?access_token=a68970d0e2205f734085612c6998864010927d9f79f6a8867b8d6aad5a266b10"
